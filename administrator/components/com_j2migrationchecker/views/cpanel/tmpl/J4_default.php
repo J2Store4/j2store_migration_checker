e831d513b08cfa777e3f84943303731671420ee8
thomas <thomastomy2001@gmail.com>
2023-09-15 11:39:41 +0530

    - add joomla4 default view file in J2MigrationChecker.

---
+++ administrator/components/com_j2migrationchecker/views/cpanel/tmpl/J4_default.php
@@ -0,0 +1,8 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+?>
+
+<div class="alert alert-info  center">
+    <h4 class="alert-heading"><?php  echo  JText::_('COM_J2STOREMIGRATIONCHECK_NOT_NEED_IN_JOOMLA4') ; ?></h4>
+</div>
+
